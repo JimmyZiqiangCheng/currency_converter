61f5f030bf13176bc423d6ec41f48adda87d3d03
Ziqiang Cheng <ziqiangcheng@Ziqiangs-MacBook-Pro.local>
2020-02-27 16:47:38 +1030

    solved navi bar routing using react router with react bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
 // src/components/NavBar.js
 
-import React, { useState } from "react";
+import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
-import { Link } from "react-router-dom";
-import { NavLink as RouterNavLink } from "react-router-dom";
-import { Nav, Navbar, Button } from 'react-bootstrap';
+import { NavLink as RouterNavLink, Link} from "react-router-dom";
+import { Nav, Navbar, Button } from "react-bootstrap";
+
 
 const NavigationBar = () => {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -15,17 +15,13 @@ const NavigationBar = () => {
 
   return(
     <Navbar bg='dark' variant='dark' expand='md'>
-    <Navbar.Brand id="logo" href="/"> Jimmys </Navbar.Brand>
+    <Navbar.Brand id = "logo" as={Link} to="/"> Jimmys </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
     <Navbar.Collapse id="basic-navbar-nav">
       {!isAuthenticated &&(
         <Nav className="ml-auto">
         <Nav.Item>
-          <Button 
-            className="logButton" 
-            variant="outline-info" 
-            onClick = {()=>loginWithRedirect({})}
-          >
+          <Button className="logButton" variant="outline-info" onClick = {()=>loginWithRedirect({})}>
             Log In
           </Button>
         </Nav.Item>
@@ -33,16 +29,12 @@ const NavigationBar = () => {
       )}
       {isAuthenticated && (
         <Nav className="ml-auto">
-          <Nav.Item><Nav.Link href='home'>Home</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link href='profile'>Profile</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link href='currency'>Currency Converter</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link href='weather'>Weather</Nav.Link></Nav.Item>
+          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+          <Nav.Link as={Link} to="/currency">Currency Converter</Nav.Link>
+          <Nav.Link as={Link} to="/weather">Weather</Nav.Link>
           <Nav.Item>
-          <Button 
-            className="logButton" 
-            variant="outline-info" 
-            onClick={()=>logoutWithRedirect()}
-          >
+          <Button className="logButton" variant="outline-info" onClick={()=>logoutWithRedirect()}>
             Log Out
           </Button>
         </Nav.Item>
